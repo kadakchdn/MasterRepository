f2b3f462277ed7b612670a6572bd90e1b8f0ffd0
Akshay <kambleakshay163@.com>
2022-11-02 11:39:07 +0530

    First Commit

---
+++ src/main/java/Pankaj/Pankaj/App.java
@@ -0,0 +1,13 @@
+package Pankaj.Pankaj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Pankaj/Pankaj/Demo1.java
@@ -0,0 +1,11 @@
+package Pankaj.Pankaj;
+
+public class Demo1 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hii");
+	}
+
+}
